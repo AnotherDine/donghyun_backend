46145051244f00be2cff8d05fb4eb7bad8eeec3f
dh.kim <dh.kim@odinue.net>
2024-01-31 20:45:22 +0900

    init

---
+++ src/main/java/com/study/donghyun_backend/DonghyunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.study.donghyun_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonghyunBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonghyunBackendApplication.class, args);
+    }
+
+}
