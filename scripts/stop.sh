78f83107f7c790192e1fc38bb9c94c924aa6a48f
dh.kim <dh.kim@odinue.net>
2024-02-07 11:47:41 +0900

    [Fix] stop.sh 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,7 +6,7 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # 현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -fl $JAR_FILE | grep java | awk '{print $1}')
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
--- src/main/java/com/study/donghyun_backend/config/CorsConfig.java
+++ src/main/java/com/study/donghyun_backend/config/CorsConfig.java
@@ -8,6 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
+    public CorsConfig() {
+        System.out.println("MyConfiguration bean is created.");
+    }
+    
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
