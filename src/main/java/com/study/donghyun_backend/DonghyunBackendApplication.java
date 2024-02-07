163f33397d6167f2caef9682b0bda43bf52f6168
dh.kim <dh.kim@odinue.net>
2024-02-07 11:25:10 +0900

    [Add] CorsConfig 수정

--- src/main/java/com/study/donghyun_backend/DonghyunBackendApplication.java
+++ src/main/java/com/study/donghyun_backend/DonghyunBackendApplication.java
@@ -2,8 +2,10 @@ package com.study.donghyun_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class DonghyunBackendApplication {
 
     public static void main(String[] args) {
